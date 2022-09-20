15e25c66c5c5d427a945e3552b2f33f37b4f3bcd
madbull12 <huangandrian02@gmail.com>
2022-09-21 00:03:24 +0700

    small changes

--- components/HoverEdit.tsx
+++ components/HoverEdit.tsx
@@ -6,11 +6,14 @@ const HoverEdit = () => {
   const[isHover,setIsHover] = useState(false);
   return (
     <div className=' absolute z-50  h-full w-full rounded-lg ' onMouseEnter={()=>setIsHover(true)} onMouseLeave={()=>setIsHover(false)} >
-      <div className='w-full h-full bg-black opacity-50 rounded-lg'>
-        <div className='bg-white opacity-1 absolute right-0 mx-auto bottom-4 left-0 w-8 h-8 rounded-full text-black grid place-items-center'>
-            <MdEdit />
+      {isHover && (
+        <div className='w-full h-full bg-black opacity-50 rounded-lg'>
+          <div className='bg-white opacity-1 absolute right-0 mx-auto bottom-4 left-0 w-8 h-8 rounded-full text-black grid place-items-center'>
+              <MdEdit />
+          </div>
         </div>
-      </div>
+      )}
+  
     
     </div>
   )
