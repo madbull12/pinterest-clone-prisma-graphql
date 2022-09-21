680e2ed3ffd9195eed6f22d0236a8ea3e1a328e2
madbull12 <huangandrian02@gmail.com>
2022-09-21 13:27:03 +0700

    edit modal changes

--- components/BoardDropdown.tsx
+++ components/BoardDropdown.tsx
@@ -3,10 +3,16 @@ import { useUser } from '@auth0/nextjs-auth0'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import { AiFillLock } from 'react-icons/ai'
+import savePin from '../helper/savePin'
 import { IBoard } from '../interface'
 import { UserIdQuery,UserBoardsQuery } from '../lib/query'
+import Button from './Button'
 
-const BoardItem = ({ board }: { board: IBoard }) => {
+interface IBoardItem {
+    board:IBoard;
+    edit?:boolean;
+}
+const BoardItem = ({ board,edit }: IBoardItem) => {
     const [showSaveBtn, setShowSaveBtn] = useState<boolean>(false);
     const router = useRouter();
     const { user } = useUser()
@@ -24,15 +30,38 @@ const BoardItem = ({ board }: { board: IBoard }) => {
         onMouseLeave={() => setShowSaveBtn(false)}
       >
         <div className="flex items-center gap-x-2">
-          <div className="bg-gray-300 w-12 h-12 rounded-lg"></div>
+            {!edit && (
+                <div className="bg-gray-300 w-12 h-12 rounded-lg"></div>
+
+            )}
           <p className="font-semibold">{board.name}</p>
         </div>
-        {!showSaveBtn && (
-          <>
-            {board.secret && <AiFillLock className="text-lg" />}
-          
-          </>
-  
+    
+            {!edit ? (
+                <>
+                    {!showSaveBtn && (
+                        <>
+                            {board.secret && (
+                                <AiFillLock />
+                            )}
+                        </>
+                      
+                    )}
+                </>
+            ):(
+                <>
+                    {board.secret && (
+                        <AiFillLock />
+                    )}
+                </>
+               
+            )}
+      
+
+        {!edit && (
+            <Button text='Save' handleClick={()=>{
+                savePin(userId?.user.id,board.id,pinId)
+            }} />
         )}
 
       </div>
@@ -55,9 +84,9 @@ const BoardDropdown = () => {
 
     console.log(userBoards?.userBoards)
   return (
-    <div>
+    <div className='p-2 rounded-lg border-gray-200 border '>
         {userBoards?.userBoards.map((board:IBoard)=>(
-            <BoardItem board={board} />
+            <BoardItem board={board} edit={true} />
         ))}
     </div>
   )
--- components/EditModal.tsx
+++ components/EditModal.tsx
@@ -1,22 +1,43 @@
-import React from 'react'
-import BoardDropdown from './BoardDropdown'
+import { useQuery } from "@apollo/client";
+import { useUser } from "@auth0/nextjs-auth0";
+import React from "react";
+import { MdArrowDropDown } from "react-icons/md";
+import { firstBoardQuery, UserIdQuery } from "../lib/query";
+import BoardDropdown from "./BoardDropdown";
 
 const EditModal = () => {
+  const { user } = useUser();
+  const { data: userId } = useQuery(UserIdQuery, {
+    variables: {
+      userId: user?.email,
+    },
+  });
+  const { data: firstBoard } = useQuery(firstBoardQuery, {
+    variables: {
+      userId: userId?.user.id,
+    },
+  });
+
+  console.log(firstBoard);
 
   return (
-    <div className='max-w-4xl mx-auto bg-white rounded-2xl p-4'>
-        <h1 className="text-center font-semibold text-2xl">Edit this Pin</h1>
-        <form className='flex gap-x-4'>
-            <div>
-                <div className='flex items-center justify-between'>
-                    <label>Board</label>
-                    <BoardDropdown />
+    <div className="max-w-4xl mx-auto bg-white rounded-2xl p-4">
+      <h1 className="text-center font-semibold text-2xl">Edit this Pin</h1>
+      <form className="flex gap-x-4 w-full mt-8">
+        <div className="w-full">
+          <div className="flex items-center justify-between">
+            <label className="flex-[0.25]">Board</label>
+            <div className="flex justify-between  items-center bg-gray-300 px-4 py-2 rounded-lg flex-[0.75]">
+              <p className="font-semibold">{firstBoard?.firstUserBoard.name}</p>
+              <MdArrowDropDown />
 
-                </div>
             </div>
-        </form>
+            {/* <BoardDropdown /> */}
+          </div>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default EditModal
+export default EditModal;
