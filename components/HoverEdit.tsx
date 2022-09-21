5bfe283c75631e2941c42da34f0ee706d7219a4a
madbull12 <huangandrian02@gmail.com>
2022-09-21 10:05:24 +0700

    hover edit style changes

--- components/HoverEdit.tsx
+++ components/HoverEdit.tsx
@@ -7,7 +7,7 @@ const HoverEdit = () => {
   return (
     <div className=' absolute z-50  h-full w-full rounded-lg ' onMouseEnter={()=>setIsHover(true)} onMouseLeave={()=>setIsHover(false)} >
       {isHover && (
-        <div className='w-full h-full bg-black opacity-50 rounded-lg'>
+        <div className='w-full h-[98%] bg-black opacity-50 rounded-2xl'>
           <div className='bg-white opacity-1 absolute right-0 mx-auto bottom-4 left-0 w-8 h-8 rounded-full text-black grid place-items-center'>
               <MdEdit />
           </div>
