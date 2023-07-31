5e0c33f7ff68f71a725fcc2ffc37789db51f6749
madbull12 <huangandrian02@gmail.com>
2023-07-31 18:03:42 +0700

    comment user link

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { IBoard, IComment } from '../interface';
 
 import Image from 'next/image';
+import Link from 'next/link';
 interface IProps {
     comment: IComment;
   }
 const Comment = ({comment}:IProps) => {
   return (
-    <div  className={`flex gap-x-2 items-center`}>
+    <Link href={`/user/${comment.userId}/created`}>
+    <div  className={`flex gap-x-2 items-center cursor-pointer`}>
       <div className="h-6 w-6 md:h-8 md:w-8 relative">
         <Image
           className="rounded-full"
@@ -25,6 +27,8 @@ const Comment = ({comment}:IProps) => {
         <p className="text-sm md:text-base lg:text-lg">{comment.content}</p>
       </div>
     </div>
+    </Link>
+    
   )
 }
 
