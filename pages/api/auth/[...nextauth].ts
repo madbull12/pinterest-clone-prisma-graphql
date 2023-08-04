f500598b30d07b900fa96c37c5efc55fc134b54f
madbull12 <huangandrian02@gmail.com>
2023-08-04 21:44:11 +0700

    adding secret to nextauth.ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -19,6 +19,7 @@ export const authOptions:NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET
 }
 
 export default NextAuth(authOptions)
